2527819390e0a2ec23e8f8ffac21d8f835963487
TESLANET <gokhanozdemir_77@hotmail.com>
2022-06-25 14:09:32 +0300

    first Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Class from './routes/Class/Class.page';
+import Functional from './routes/Functional/Functional.page';
+import Home from './routes/Home/Home.page';
+import Navigation from './routes/Navigation/Navigation.page';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="function" element={<Functional />} />
+          <Route path="class" element={<Class />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
